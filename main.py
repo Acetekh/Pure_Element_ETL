fe29984fbc7890d299a800e8d2f0ac0fe6bfee61
Ace-tech1 <Philipclement73@yahoo.com>
2026-04-12 08:47:15 +0100

    Pure Element ETL Pipeline with PostgreSQL

---
+++ main.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from sqlalchemy import create_engine
+import os
+from dotenv import load_dotenv
+
+# 1. SECURITY 
+load_dotenv()
+db_pass = os.getenv('DB_PASSWORD')
+db_url = f'postgresql://postgres:{db_pass}@localhost:5432/vault_production'
+
+# 2. EXTRACT
+df = pd.read_csv('vault.csv')
+
+# 3. TRANSFORM 
+def categorize_item(item):
+    if 'Gold' in item or 'Diamond' in item: return 'Fine Jewelry'
+    if 'Watch' in item or 'Rolex' in item: return 'Luxury Timepieces'
+    return 'General'
+
+df['category'] = df['item_type'].apply(categorize_item)
+
+
+# 4. LOAD 
+engine = create_engine(db_url)
+
+# This sends the data to a table name in my DBeaver
+df.to_sql('vault_sales', engine, if_exists='replace', index=False)
+
+print("Pure Element raw data record is now in PostgreSQL")
+
+
